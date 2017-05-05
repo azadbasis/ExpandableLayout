84f695f443ed2a2bf504a31aab1f30c48e996286
Azhar <azadbasis@gmail.com>
2017-05-06 00:04:26 +0600

    Android Master Detail ListView CRUD - ADD UPDATE DELETE [Open Activity]

--- RecyclerViewSwipeToOpenActivityandPassdata/app/src/main/java/com/recyclerview/azhar/recyclerviewswipetoopenactivityandpassdata/adapter/OnItemTouch.java
+++
@@ -1,9 +0,0 @@
-package com.recyclerview.azhar.recyclerviewswipetoopenactivityandpassdata.adapter;
-
-/**
- * Created by Azhar on 5/1/2017.
- */
-
-public interface OnItemTouch {
-    void touchItem(int position);
-}
